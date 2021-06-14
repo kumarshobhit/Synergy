314a6993629f9b47904dc90a4502314599ac4e70
unknown <ishobhit3@gmail.com>
2021-06-15 00:19:35 +0530

    fixed one issue

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT=process.env.PORT || 5000
 
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/users',require('./routes/users'))
+app.use('/api/contacts',require('./routes/contacts'))
+
 //Server static assest in production
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
@@ -23,8 +27,6 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 // Define Routes
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/users',require('./routes/users'))
-app.use('/api/contacts',require('./routes/contacts'))
+
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
